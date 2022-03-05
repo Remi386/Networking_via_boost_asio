911ea9f380050c88e2ec70c8b6d5f9bcc89b68a5
Remi386 <kipcramil@gmail.com>
2022-03-05 17:12:26 +0300

    asynchronous requests

--- asio_test/Source.cpp
+++ asio_test/Source.cpp
@@ -6,17 +6,37 @@
 #include <vector>
 #include <thread>
 #include <Boost/asio.hpp>
+#include <Boost/bind.hpp>
 using namespace boost;
 using namespace std::chrono_literals;
+std::vector<uint8_t> mBuff(1024 * 20);
+
+void TakeData(asio::ip::tcp::socket& sock) {
+	sock.async_read_some(asio::buffer(mBuff.data(), mBuff.size()),
+		[&](system::error_code err, size_t length) 
+		{
+			if (err) {
+				std::cout << err.what() << std::endl;
+			}
+			else {
+				std::cout << "Read " << length << " bytes\n";
+				for (int i = 0; i < length; ++i) {
+					std::cout.put(mBuff[i]);
+				}
+				std::cout << std::endl;
+			}
+		}
+	);
+}
+
 
 int main()
 {
 	asio::io_context context;
-
 	asio::ip::tcp::socket sock(context);
 	system::error_code err;
 	asio::ip::tcp::resolver resolver(context);
-	
+
 	std::string url;      // = "www.yandex.ru"; // = "example.com"
 	do {
 
@@ -33,7 +53,7 @@ int main()
 			std::cout << err.what() << std::endl;
 			continue;
 		}
-
+		//we are trying to connect only to the first adress in list given by resolver
 		sock.connect(*iter, err);
 
 		if (err) {
@@ -46,25 +66,16 @@ int main()
 			"Host: " + url + "\r\n"
 			"Connection: close\r\n\r\n";
 
+		context.post(boost::bind(TakeData, boost::ref(sock))); //adding async task
+
 		sock.write_some(boost::asio::buffer(request.data(), request.size()));
 
-		std::this_thread::sleep_for(200ms); //waiting for response
-
-		size_t bytes = sock.available();
-
-		if (bytes > 0) {
-			std::vector<uint8_t> buff(bytes);
-			sock.read_some(boost::asio::buffer(buff.data(), buff.size()));
-
-			for (int i = 0; i < buff.size(); ++i) {
-				std::cout.put(buff[i]);
-			}
-			std::cout << std::endl;
-
-		}
+		context.run(); //blocking until all async task are finished
+		context.restart(); //preparing for next 'run' call
 
 		std::cout << "\nRequest done!" << std::endl;
 		sock.close();
+
 	} while (true);
 
 
